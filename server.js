3784fffa8d7b69fa43e21a8420161130670d1120
Brayan ODURO <brayan.oduro@gmail.com>
2018-04-18 06:58:46 +0200

    popo

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 //Install express server
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000
+
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-app.listen(process.env.PORT, function () {
-    console.log('Example app listening on port 8080!')
-  })
