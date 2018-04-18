3d174487596f4b302aa72f6ae26a5bba57f1e36f
Brayan ODURO <brayan.oduro@gmail.com>
2018-04-18 05:48:53 +0200

    test

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT);
 
-app.listen(process.env.PORT || 8080, function () {
+app.listen(process.env.PORT, function () {
     console.log('Example app listening on port 8080!')
   })
